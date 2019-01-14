7f3d38d550b88578b861957f3b6ba2dc09903e72
israelcena <israelsjm@gmail.com>
2019-01-14 09:02:44 -0400

    install npm pack for del gulp-sass gulp-html and gulp-notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
+// Vars
+
+var html = require('gulp-htmlmin');
+
+// htmlmin
+
+gulp.task('html', function(){
+  return gulp.src('./src/index.html')
+    .pipe(html({collapseWhitespace:true}))
+    .pipe(gulp.dest('./dist/'));
+});
+
+// defaultTask
+
 function defaultTask(cb) {
     // place code for your default task here
     cb();
